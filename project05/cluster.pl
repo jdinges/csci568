f9fe08df38849ba9fbf96e21c2b488fc63ce080a
Josh Dinges <jdinges@mines.edu>
2011-09-29 02:35:14 -0600

    Got most of the clustering working...

--- project05/cluster.pl
+++ project05/cluster.pl
@@ -24,55 +24,118 @@ sub KMeans
 	my @data = @{$listRef};
 	my $datasize = @data;
 	my @clusterList = ();
+	# Calculate maximums for each attribute
+	my @maximums;
+	for(my $i = 0; $i<NUMBER_ATTRIBUTES; $i++){
+		my @localmaximums = ();
+		for(my $j = 0; $j<$datasize; $j++){
+			#print"data[$j]->[$i] = $data[$j]->[$i]\n";
+			push(@localmaximums,$data[$j]->[$i]);
+		}
+		my $max = (sort {$a <=> $b} @localmaximums)[-1];
+		#print"Max for attribute $i = $max\n";
+		push(@maximums,$max);
+	}
+	
 	# Generate K random data points
 	my @karray = ();
 	for($i = 0; $i<$k; $i++){
 		my @randomAttributes = ();
 		for($j = 0; $j<NUMBER_ATTRIBUTES; $j++){
-			my $newnumber = rand(MAX_LENGTH);
+			my $newnumber = rand($maximums[$j]);
 			push(@randomAttributes, $newnumber);
 		}
 		push(@karray, [@randomAttributes]);
 	}
 	
-	# Calculate the euclidean distance for each point compared to each centroid
-	my @clusters = ();
-	for(my $i = 0; $i < $k; $i++){
-		my @keuclidean = ();
-		my $comparek = $karray[$i];
-		#print"For k = $comparek->[0],$comparek->[1],$comparek->[2],$comparek->[3]\n";
-		foreach my $datapoint (@data){
-			my $eValue = &euclideanDistance($datapoint, $comparek);
-			#print"Euclidean Distance of $datapoint->[0], $datapoint->[1], $datapoint->[2], $datapoint->[3] = $eValue\n";
-			push(@keuclidean,$eValue);
+	for(my $h = 0; $h < 5; $h++){
+		
+		# Calculate the euclidean distance for each point compared to each centroid
+		my @clusters = ();
+		for(my $i = 0; $i < $k; $i++){
+			my @keuclidean = ();
+			my $comparek = $karray[$i];
+			print"For k = $comparek->[0],$comparek->[1],$comparek->[2],$comparek->[3]\n";
+			foreach my $datapoint (@data){
+				my $eValue = &euclideanDistance($datapoint, $comparek);
+				#print"Euclidean Distance of $datapoint->[0], $datapoint->[1], $datapoint->[2], $datapoint->[3] = $eValue\n";
+				push(@keuclidean,$eValue);
+			}
+			#print"\n";
+			push(@clusters,[@keuclidean]);
 		}
-		#print"\n";
-		push(@clusters,[@keuclidean]);
-	}
 	
-	for(my $i = 0; $i < $datasize; $i++){
-		my @comparable = ();
-		foreach my $kEuclid (@clusters){
-			push(@comparable, $kEuclid->[$i]);
+		# Organize into clusters. Use cluster list. The index of clusterList maps to the data point
+		# and the value at that index maps to which cluster the data point belongs in.
+		for(my $i = 0; $i < $datasize; $i++){
+			my @comparable = ();
+			foreach my $kEuclid (@clusters){
+				push(@comparable, $kEuclid->[$i]);
+			}
+			my @ordered = sort {$a <=> $b} (@comparable);
+			#print"$ordered[0], $ordered[1] VS. $comparable[0], $comparable[1]\n";
+			my $target = $ordered[0];
+			my $index = -1;
+			for(my $j = 0; $j < $k; $j++){
+				if($comparable[$j] == $target){
+					#print"match! $comparable[$j] = $target\t => index = $j\n";
+					$index = $j;
+					last;
+				}
+			}
+			push(@clusterList, $index);
 		}
-		my @ordered = sort {$a <=> $b} (@comparable);
-		#print"$ordered[0], $ordered[1] VS. $comparable[0], $comparable[1]\n";
-		my $target = $ordered[0];
-		my $index = -1;
-		for(my $j = 0; $j < $k; $j++){
-			if($comparable[$j] == $target){
-				print"match! $comparable[$j] = $target\t => index = $j\n";
-				$index = $j;
-				last;
+	
+		my $clusterSize = @clusterList;
+		for(my $i = 0; $i<$datasize; $i++){
+			print"Data Point = $data[$i]->[0], $data[$i]->[1], $data[$i]->[2], $data[$i]->[3]\t => \t#$clusterList[$i]\n";
+		}
+		
+		# cluster
+		for(my $i = 0; $i < $k; $i++){
+			my @totalcluster = (0, 0, 0, 0);
+			my $clustercount = 0;
+			# cluster-data map
+			for(my $j = 0; $j < $clusterSize; $j++){
+				my $element = $clusterList[$j]; 
+				if($element == $i){
+					$clustercount++;
+					my @dataArray = @{$data[$j]};
+					my $dataArraySize = @dataArray;
+					
+					# data
+					for(my $a = 0; $a<$dataArraySize; $a++){
+						my $attribute = $dataArray[$a];
+						if($attribute=~/\d+/){
+							$totalcluster[$a] = $totalcluster[$a] + $attribute;
+							#print"Cluster $i, Data Point $j, totalcluster[$a] = $totalcluster[$a]\n";
+						} elsif ($clustercount) {
+							print"no attribute increase\n";
+						}
+					}
+				}
+			}
+			# average
+			my $totalClusterSize = @totalcluster;
+			print"Cluster #$i centroid = ";
+			if($clustercount != 0){
+				for(my $b = 0; $b<$totalClusterSize; $b++){
+					my $totalattribute = $totalcluster[$b];
+					if($totalattribute=~/\d+/){
+						$totalcluster[$b] = $totalattribute / $clustercount; 
+						print"$totalcluster[$b], ";
+					}
+				}
+				print"\n";
+				# update centroid
+				$karray[$i] = [@totalcluster];
+			} else {
+				print"$karray[$i]->[0], $karray[$i]->[1], $karray[$i]->[2], $karray[$i]->[3] ... NO MATCH";
+				print"\n";
 			}
 		}
-		push(@clusterList, $index);
 	}
 	
-	my $clusterSize = @clusterList;
-	for(my $i = 0; $i<$datasize; $i++){
-		print"Data Point = $data[$i]->[0], $data[$i]->[1], $data[$i]->[2], $data[$i]->[3]\t => \t#$clusterList[$i]\n";
-	}
 }
 
 sub euclideanDistance
