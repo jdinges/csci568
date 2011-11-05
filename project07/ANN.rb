c78ec2f1eacf369b7f4a3b51e81f463f8d887148
Josh Dinges <jdinges@mines.edu>
2011-11-04 23:06:18 -0600

    Nevermind. Class variables were a bad idea. Now they're instance variables.

--- project07/ANN.rb
+++ project07/ANN.rb
@@ -1,5 +1,5 @@
 class Network
-  @@layers = Array.new
+  @layers = Array.new
   
   def initialize(layer_count)
     puts "Creating network..."
@@ -9,64 +9,71 @@ class Network
       puts "Great. And what kind of layer is this?"
       layer_type = gets
       layer = Layer.new(node_count, layer_type)
-      @@layers.push(layer)
+      @layers.push(layer)
     end
   end
   
   def self.layers
-    @@layers
+    @layers
   end
+  
+  def self.run
+    for i in 0..@layers.length
+      thisLayer = @layers[i]
+      for j in 0..thisLayer.length
+        
   
 end
 
 class Layer
-  @@nodes = Array.new
-  @@name
+  @nodes = Array.new
+  @name
   
   def initialize(node_count, layer_name)
-    @@name = layer_name
+    @name = layer_name
     puts "Creating layer..."
     for i in 0..node_count
       puts "What is the value of the node?"
       node_value = gets
       node = Node.new(node_value)
-      @@nodes.push(node)
+      @nodes.push(node)
     end
   end
   
   def self.nodes
-    @@nodes
+    @nodes
   end
   
 end
 
 class Node
-  @@value
-  @@edges = Array.new
+  @value
+  @edges = Array.new
   
   def intialize(my_weight)
-    @@value = my_weight
+    @value = my_weight
   end
   
   def self.addEdge(edge)
-    @@edges.push(edge)
+    @edges.push(edge)
   end
   
   def self.value
-    @@value
+    @value
   end
   
 end
 
 class Edge
-  @@weight
+  @weight
   
   def initialize(my_weight)
-    @@weight = my_weight
+    @weight = my_weight
   end
   
   def self.weight
-    @@weight
+    @weight
   end
 end
 
+# Main
