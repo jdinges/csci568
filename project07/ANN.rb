5da93f45a797e41383bb999cdb8012fc3bba8444
Josh Dinges <jdinges@mines.edu>
2011-11-07 07:10:14 -0700

    Most features work. Network almost built.

--- project07/ANN.rb
+++ project07/ANN.rb
@@ -1,6 +1,6 @@
 class Network
   
-  #@layers = Array.new
+  attr_accessor :layers
   
   def initialize(layer_count)
     @layers = Array.new()
@@ -13,19 +13,20 @@ class Network
     end
   end
   
-  def self.layers
-    @layers
-  end
+  #def self.layers
+   # @layers
+  #end
   
-  def self.run
+  def run
     for i in 0..@layers.length-1
-      
       thisLayer = @layers[i]
+      puts "For layer #{i}: There are #{thisLayer.nodes.length} nodes"
+      
       theseNodes = thisLayer.nodes
       
       for j in 0..theseNodes.length-1
         
-        thisNode = theseNode[j]
+        thisNode = theseNodes[j]
         theseEdges = thisNode.edges
         
         sum = 0;
@@ -33,7 +34,7 @@ class Network
         for k in 0..theseEdges.length-1
           
           thisEdge = theseEdges[k]
-          sum += thisEdge * thisNode.value
+          sum += thisEdge.weight * thisNode.value
           
         end
         
@@ -54,33 +55,41 @@ end
 
 class Layer
   
-  attr_accessor :name
+  attr_accessor :name, :nodes
   
   def initialize(layer_name)
     @name = layer_name
     @nodes = Array.new
   end
   
-  def self.addNode(node)
+  def addNode(node)
     @nodes.push(node)
   end
   
-  def self.nodes
-    @nodes
+  def nodes
+    if @nodes.length > 0
+      return @nodes
+    else
+      return Array.new
+    end
+  end
+  
+  def self.push(node)
+    @nodes<<node
   end
   
 end
 
 class Node
   
-  attr_accessor :value
+  attr_accessor :value, :edges
   
-  def intialize(my_weight)
-    @value = my_weight
+  def initialize(my_value)
+    @value = my_value
     @edges = Array.new
   end
   
-  def self.addEdge(edge)
+  def addEdge(edge)
     @edges.push(edge)
   end
   
@@ -100,3 +109,32 @@ end
 
 # Main
 theNetwork = Network.new(3)
+
+# Set up network layout
+
+# Input layer...
+
+# Create nodes...
+iNode1 = Node.new(0)
+iNode2 = Node.new(0)
+iNode3 = Node.new(0)
+
+# Create edges
+iEdge1 = Edge.new(1.0,nil,iNode1)
+iEdge2 = Edge.new(0.25,nil,iNode2)
+iEdge3 = Edge.new(-0.5,nil,iNode3)
+
+iNode1.addEdge(iEdge1)
+iNode2.addEdge(iEdge2)
+iNode3.addEdge(iEdge3)
+
+# Add to network...
+theNetwork.layers[0].addNode(iNode1)
+theNetwork.layers[0].addNode(iNode2)
+theNetwork.layers[0].addNode(iNode3)
+
+
+
+
+# Run
+theNetwork.run
