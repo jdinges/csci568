81c92c9d94c862ede91411565854b8435bd94b89
Josh Dinges <jdinges@mines.edu>
2011-11-07 22:55:08 -0700

    Added random weights. Everything should be set up. Now I just need to implement train and backpropegate...

--- project07/ANN.rb
+++ project07/ANN.rb
@@ -18,9 +18,9 @@ class Network
   #end
   
   def run
-    for i in 0..@layers.length-1
+    for i in 1..@layers.length-1
       thisLayer = @layers[i]
-      puts "For layer #{i}: There are #{thisLayer.nodes.length} nodes"
+      puts "For layer #{thisLayer.name}: There are #{thisLayer.nodes.length} nodes"
       
       theseNodes = thisLayer.nodes
       
@@ -34,11 +34,12 @@ class Network
         for k in 0..theseEdges.length-1
           
           thisEdge = theseEdges[k]
-          sum += thisEdge.weight * thisNode.value
+          puts "\t#{sum} += #{thisEdge.weight * thisEdge.source.value}"
+          sum += thisEdge.weight * thisEdge.source.value
           
         end
         
-        puts "Node #{j} new value = #{sum}"
+        puts "\tNode #{j} new value = #{sum}"
         @layers[i].nodes[j].value = sum
         
         if i == @layers.length-1
@@ -108,47 +109,50 @@ class Edge
 
 end
 
+class Range
+  def rand
+    return self.begin + Kernel.rand(self.end-self.begin)
+  end
+end
+
 # Main
 theNetwork = Network.new(3)
 
 # Set up network layout
 
-# Input layer...
+#----------------#
+# Input layer... #
+#----------------#
 
 # Create nodes...
-iNode1 = Node.new(1)
-iNode2 = Node.new(1)
-iNode3 = Node.new(1)
-
-# Create edges
-iEdge1 = Edge.new(1.0,nil,iNode1)
-iEdge2 = Edge.new(0.25,nil,iNode2)
-iEdge3 = Edge.new(-0.5,nil,iNode3)
-
-iNode1.addEdge(iEdge1)
-iNode2.addEdge(iEdge2)
-iNode3.addEdge(iEdge3)
+iNode1 = Node.new(1.0)
+iNode2 = Node.new(0.25)
+iNode3 = Node.new(-0.5)
 
 # Add to network...
 theNetwork.layers[0].addNode(iNode1)
 theNetwork.layers[0].addNode(iNode2)
 theNetwork.layers[0].addNode(iNode3)
 
-# Hidden layer...
+#-----------------#
+# Hidden layer... #
+#-----------------#
 
 # Create nodes...
-hNode1 = Node.new(1)
-hNode2 = Node.new(1)
+hNode1 = Node.new(0)
+hNode2 = Node.new(0)
 
 # Creates edges
-hEdge1_1 = Edge.new(1.0, iNode1, hNode1)
-hEdge1_2 = Edge.new(1.0, iNode1, hNode2)
+generator = 2.0*rand-1
+puts "rand = #{rand}"
+hEdge1_1 = Edge.new(2.0*rand-1, iNode1, hNode1)
+hEdge1_2 = Edge.new(2.0*rand-1, iNode1, hNode2)
 
-hEdge2_1 = Edge.new(1.0, iNode2, hNode1)
-hEdge2_2 = Edge.new(1.0, iNode2, hNode2)
+hEdge2_1 = Edge.new(2.0*rand-1, iNode2, hNode1)
+hEdge2_2 = Edge.new(2.0*rand-1, iNode2, hNode2)
 
-hEdge3_1 = Edge.new(1.0, iNode3, hNode1)
-hEdge3_2 = Edge.new(1.0, iNode3, hNode2)
+hEdge3_1 = Edge.new(2.0*rand-1, iNode3, hNode1)
+hEdge3_2 = Edge.new(2.0*rand-1, iNode3, hNode2)
 
 # Add edges to Nodes...
 hNode1.addEdge(hEdge1_1)
@@ -163,21 +167,23 @@ hNode2.addEdge(hEdge3_2)
 theNetwork.layers[1].addNode(hNode1)
 theNetwork.layers[1].addNode(hNode2)
 
-# Output layer...
+#-----------------#
+# Output layer... #
+#-----------------#
 
 # Create nodes
-oNode1 = Node.new(1)
-oNode2 = Node.new(1)
-oNode3 = Node.new(1)
+oNode1 = Node.new(0)
+oNode2 = Node.new(0)
+oNode3 = Node.new(0)
 
 #  Create edges
-oEdge1_1 = Edge.new(1.0, hNode1, oNode1)
-oEdge1_2 = Edge.new(1.0, hNode1, oNode2)
-oEdge1_3 = Edge.new(1.0, hNode1, oNode3)
+oEdge1_1 = Edge.new(2.0*rand-1, hNode1, oNode1)
+oEdge1_2 = Edge.new(2.0*rand-1, hNode1, oNode2)
+oEdge1_3 = Edge.new(2.0*rand-1, hNode1, oNode3)
 
-oEdge2_1 = Edge.new(1.0, hNode2, oNode1)
-oEdge2_2 = Edge.new(1.0, hNode2, oNode2)
-oEdge2_3 = Edge.new(1.0, hNode2, oNode3)
+oEdge2_1 = Edge.new(2.0*rand-1, hNode2, oNode1)
+oEdge2_2 = Edge.new(2.0*rand-1, hNode2, oNode2)
+oEdge2_3 = Edge.new(2.0*rand-1, hNode2, oNode3)
 
 # Add edges to nodes...
 oNode1.addEdge(oEdge1_1)
